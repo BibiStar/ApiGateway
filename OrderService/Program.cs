0b618812b04c7f13fc577a3a6d34dea54dd7c022
Bianca Nunes <biancanunes@ghc.com.br>
2025-04-02 14:04:39 -0300

    Adicionados serviços Users e Orders

---
+++ OrderService/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/orders", () => new[] { new { Id = 101, Product = "Notebook" }, new { Id = 102, Product = "Smartphone" } });
+
+app.Run();
